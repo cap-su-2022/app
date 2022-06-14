29dd29dccebc8a936c1f717e4b57e82c469cfeba
longdemon <longnv031120@gmail.com>
2022-06-14 10:48:30 +0700

    Fix error in change-password.component

--- apps/frontend/components/preferences/change-password.component.tsx
+++ apps/frontend/components/preferences/change-password.component.tsx
@@ -3,317 +3,125 @@ import {
   createStyles,
   PasswordInput,
   Text,
-  TextInput
-} from "@mantine/core";
-import { useFormik } from "formik";
-import * as React from "react";
-import * as Yup from "yup";
-import { Check, X } from "tabler-icons-react";
-import { useAppDispatch } from "../../redux/hooks";
-import { showNotification } from "@mantine/notifications";
-import { changePassword } from "../../redux/features/account/thunk/change-password.thunk";
-
-======
-=
-  TextInput;
-}
-from;
-"@mantine/core";
-import { useFormik } from "formik";
-import * as React from "react";
-import * as Yup from "yup";
-import { Check, X } from "tabler-icons-react";
-import { useAppDispatch } from "../../redux/hooks";
-import { showNotification } from "@mantine/notifications";
-import { changePassword } from "../../redux/features/account/thunk/change-password.thunk";
-
->>>>>>>
-origin / develop;
+  TextInput,
+} from '@mantine/core';
+import { useFormik } from 'formik';
+import * as React from 'react';
+import * as Yup from 'yup';
+import { Check, X } from 'tabler-icons-react';
+import { useAppDispatch } from '../../redux/hooks';
+import { showNotification } from '@mantine/notifications';
+import { changePassword } from '../../redux/features/account/thunk/change-password.thunk';
 
 export default function ChangePassword({ username }) {
   const { classes } = useStyles();
   const initialFormValues = {
-    newPass: "",
-    confirmPass: ""
+    newPass: '',
+    confirmPass: '',
   };
 
   const dispatch = useAppDispatch();
 
   const ChangePassSchema = Yup.object().shape({
     newPass: Yup.string()
-      .min(5, "Password must be between 5-50 characters")
-      .max(50, "Password must be between 5-50 characters")
-      .required("Password is required"),
+      .min(5, 'Password must be between 5-50 characters')
+      .max(50, 'Password must be between 5-50 characters')
+      .required('Password is required'),
     confirmPass: Yup.string().oneOf(
-      [Yup.ref("newPass"), null],
-      "Passwords must match"
-    )
+      [Yup.ref('newPass'), null],
+      'Passwords must match'
+    ),
   });
 
   const handleChangePassSubmit = async (value) => {
     dispatch(
       changePassword({
-    <
-    <
-    <
-    <
-    <
-    << HEAD
-      password: value.newPass,
-  ======
-    =
-      password;
-  :
-    value.newPass
-    >>> >>> > origin / develop;
-  })
-  )
-  .
-    unwrap()
+        password: value.newPass,
+      })
+    )
+      .unwrap()
       .then(() =>
         showNotification({
-    <
-    <
-    <
-    <
-    <
-    << HEAD
-      id: 'load-data',
-          color: "teal",
-            title;
-  :
-    "Data was updated",
-      message;
-  :
-    "Your profile was updated successfully",
-      icon;
-  :
-    <Check />,
-      autoClose;
-  :
-    3000,
-  ======
-    =
-      id;
-  :
-    "load-data",
-      color;
-  :
-    "teal",
-      title;
-  :
-    "Data was updated",
-      message;
-  :
-    "Your profile was updated successfully",
-      icon;
-  :
-    <Check />,
-      autoClose;
-  :
-    3000
-    >>> >>> > origin / develop;
-  })
-  )
-  .
-    catch((e) => {
-      showNotification({
-      <
-      <
-      <
-      <
-      <
-      << HEAD
-        id: 'load-data',
-          color: "red",
-            title;
-    :
-      "Have error",
-        message;
-    :
-      `${e.message}`,
-        icon;
-    :
-      <X />,
-        autoClose;
-    :
-      3000,
-    ======
-      =
-        id;
-    :
-      "load-data",
-        color;
-    :
-      "red",
-        title;
-    :
-      "Have error",
-        message;
-    :
-      `${e.message}`,
-        icon;
-    :
-      <X />,
-        autoClose;
-    :
-      3000
-      >>> >>> > origin / develop;
-    })
-      ;
-    });
+          id: 'load-data',
+          color: 'teal',
+          title: 'Data was updated',
+          message: 'Your profile was updated successfully',
+          icon: <Check />,
+          autoClose: 3000,
+        })
+      )
+      .catch((e) => {
+        showNotification({
+          id: 'load-data',
+          color: 'red',
+          title: 'Have error',
+          message: `${e.message}`,
+          icon: <X />,
+          autoClose: 3000,
+        });
+      });
   };
 
   const formik = useFormik({
     initialValues: initialFormValues,
     enableReinitialize: true,
-    validationSchema: ChangePassSchema
-  <
-  <
-  <
-  <
-  <
-  << HEAD
-    onSubmit: (values;
-) =>
-  handleChangePassSubmit(values),
-======
-  =
-    onSubmit;
-:
-  (values) => handleChangePassSubmit(values)
-    >>> >>> > origin / develop;
-})
-  ;
+    validationSchema: ChangePassSchema,
+    onSubmit: (values) => handleChangePassSubmit(values),
+  });
   return (
     <form onSubmit={formik.handleSubmit}>
       <div className={classes.header}>
         <Text
           size="lg"
-        <<<<<<< HEAD
-        sx={{ textTransform: "uppercase"}}
-        =======
-        sx={{ textTransform: "uppercase"}}
-        >>>>>>> origin/develop
-           weight={500}
-           color="blue"
-      >
-        Change password for {username}
-      </Text>
-    </div>
+          sx={{ textTransform: 'uppercase' }}
+          weight={500}
+          color="blue"
+        >
+          Change password for {username}
+        </Text>
+      </div>
 
-  <PasswordInput
-    id="newPass"
-    description="Input your new password"
-  < < < < < < <
-  HEAD
-  onChange
-  = {
-  formik.handleChange
-  (
-  "newPass")
-}
-  = = = = = = =
-  onChange
-  = {
-  formik.handleChange
-  (
-  "newPass")
-}
->>>>>>> origin/develop
-error=
-  {
-    formik.touched.newPass && Boolean(formik.errors.newPass)
-      ? formik.errors.newPass
-      : null;
-  }
-value=
-  {
-    formik.values.newPass;
-  }
-<<<<<<< HEAD
-  label
-  = {
-  'New Password'
-}
-  = = = = = = =
-  label
-  = {
-  "New Password"
-}
->>>>>>> origin/develop
-required
-name="newPass"
-className=
-  {
-    classes.inputText;
-  }
-placeholder="New Password"
-/>
+      <PasswordInput
+        id="newPass"
+        description="Input your new password"
+        onChange={formik.handleChange('newPass')}
+        error={
+          formik.touched.newPass && Boolean(formik.errors.newPass)
+            ? formik.errors.newPass
+            : null
+        }
+        value={formik.values.newPass}
+        label={'New Password'}
+        required
+        name="newPass"
+        className={classes.inputText}
+        placeholder="New Password"
+      />
 
-  <PasswordInput
-    id="confirmPass"
-    description="Confirm your new password"
-  < < < < < < <
-  HEAD
-  onChange
-  = {
-  formik.handleChange
-  (
-  "confirmPass")
-}
-  = = = = = = =
-  onChange
-  = {
-  formik.handleChange
-  (
-  "confirmPass")
-}
->>>>>>> origin/develop
-error=
-  {
-    formik.touched.confirmPass && Boolean(formik.errors.confirmPass)
-      ? formik.errors.confirmPass
-      : null;
-  }
-value=
-  {
-    formik.values.confirmPass;
-  }
-<<<<<<< HEAD
-  label
-  = {
-  'Confirm Password'
-}
-  = = = = = = =
-  label
-  = {
-  "Confirm Password"
-}
->>>>>>> origin/develop
-required
-name="confirmPass"
-className=
-  {
-    classes.inputText;
-  }
-placeholder="Confirm Password"
-/>
+      <PasswordInput
+        id="confirmPass"
+        description="Confirm your new password"
+        onChange={formik.handleChange('confirmPass')}
+        error={
+          formik.touched.confirmPass && Boolean(formik.errors.confirmPass)
+            ? formik.errors.confirmPass
+            : null
+        }
+        value={formik.values.confirmPass}
+        label={'Confirm Password'}
+        required
+        name="confirmPass"
+        className={classes.inputText}
+        placeholder="Confirm Password"
+      />
 
-  <div
-    style={{
-      <<<<<<< HEAD
-      display: 'flex',
-      justifyContent: "flex-end",
-      marginTop: "50px",
-      width: "100%",
-      =======
-      display: "flex",
-      justifyContent: "flex-end",
-      marginTop: "50px",
-      width: "100%"
-      >>>>>>> origin/develop
-    }}
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          marginTop: '50px',
+          width: '100%',
+        }}
       >
         <Button color="green" type="submit" name="change">
           Save Changes
@@ -325,38 +133,12 @@ placeholder="Confirm Password"
 const useStyles = createStyles(() => {
   return {
     header: {
-  <
-  <
-  <
-  <
-  <
-  << HEAD
-    display: 'flex',
-      justifyContent: "center",
-        marginTop;
-:
-  20,
-},
-  inputText: {
-    marginTop: 10,
-  }
-,
-======
-  =
-    display;
-:
-  "flex",
-    justifyContent;
-:
-  "center",
-    marginTop;
-:
-  20;
-},
-  inputText: {
-    marginTop: 10;
-  }
->>>>>>>
-  origin / develop;
-};
+      display: 'flex',
+      justifyContent: 'center',
+      marginTop: 20,
+    },
+    inputText: {
+      marginTop: 10,
+    },
+  };
 });
