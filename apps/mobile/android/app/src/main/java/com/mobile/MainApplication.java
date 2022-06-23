931dcb0c60617871a57f56ee0672ec0071555e65
bangmaple <bangmapleproject@gmail.com>
2022-06-23 23:33:17 +0700

    update react native version

--- apps/mobile/android/app/src/main/java/com/mobile/MainApplication.java
+++ apps/mobile/android/app/src/main/java/com/mobile/MainApplication.java
@@ -8,12 +8,11 @@ import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
+import com.mobile.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.github.wumke.RNExitApp.RNExitAppPackage;
-import com.facebook.react.bridge.JSIModulePackage;
-import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -28,7 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          //packages.add(new RNExitAppPackage());
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           return packages;
@@ -38,21 +36,25 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSMainModuleName() {
           return "apps/mobile/src/main";
         }
-
-        @Override
-        protected JSIModulePackage getJSIModulePackage() {
-          return new ReanimatedJSIModulePackage();
-        }
       };
+
+  private final ReactNativeHost mNewArchitectureNativeHost =
+      new MainApplicationReactNativeHost(this);
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      return mNewArchitectureNativeHost;
+    } else {
+      return mReactNativeHost;
+    }
   }
 
   @Override
   public void onCreate() {
     super.onCreate();
+    // If you opted-in for the New Architecture, we enable the TurboModule system
+    ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
--- apps/mobile/src/app/index.tsx
+++ apps/mobile/src/app/index.tsx
@@ -1,8 +1,6 @@
-import "react-native-gesture-handler";
 import React from "react";
 import { Provider } from "react-redux";
 import App from "./App";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { makeStore } from "./redux/store";
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
 
@@ -15,8 +13,6 @@ GoogleSignin.configure({
 export const Index = () => {
   return (
     <Provider store={makeStore()}>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <App />
-      </GestureHandlerRootView>
+      <App />
     </Provider>);
 };
