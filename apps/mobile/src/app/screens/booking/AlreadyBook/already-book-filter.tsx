579df420337d2cdaf60e3c676727f303b4ee02ee
duynghim <74170051+duynghim@users.noreply.github.com>
2022-06-24 15:21:34 +0700

    Update Filter

--- apps/mobile/src/app/screens/booking/AlreadyBook/already-book-filter.tsx
+++ apps/mobile/src/app/screens/booking/AlreadyBook/already-book-filter.tsx
@@ -19,7 +19,7 @@ const AlreadyBookFilter: React.FC<AlreadyBookFilterProps> = (props) => {
     setBookingRoomType(val);
   }
   return (
-    <View style={styles.filterContainer}>
+    <View style={[styles.filterContainer, {paddingTop: 10}]}>
       <View style={{
         display: "flex",
         justifyContent: "center",
@@ -38,6 +38,9 @@ const AlreadyBookFilter: React.FC<AlreadyBookFilterProps> = (props) => {
               onChangeText={(val) => props.setSearch(val.toString())} />
           </View>
         </View>
+        <View
+          style={{width: deviceWidth/20}}
+        />
 
         <View style={{
           display: "flex",
