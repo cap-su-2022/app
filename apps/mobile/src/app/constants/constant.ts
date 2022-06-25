75773d054dce4d146e062e41ed0061c55e29cda1
cyrus <kazuocyrus@gmail.com>
2022-06-25 12:20:12 +0700

    update react native ios

--- apps/mobile/src/app/constants/constant.ts
+++ apps/mobile/src/app/constants/constant.ts
@@ -1,5 +1,5 @@
 import { Platform } from 'react-native';
 
 export const API_URL = `http://${
-  Platform.OS === 'android' ? '10.0.2.2' : '192.168.15.247'
+  Platform.OS === 'android' ? '10.0.2.2' : '10.15.99.187'
 }:5000/api/v1`;
