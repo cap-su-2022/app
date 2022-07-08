218794055c6c4dc89a29c921915b5c6288be1b8e
longdemon <longnv031120@gmail.com>
2022-07-09 00:37:49 +0700

    Update FE

--- apps/frontend/components/rooms/index.tsx
+++ apps/frontend/components/rooms/index.tsx
@@ -1,6 +1,6 @@
 import { GetServerSideProps } from 'next';
 import AdminLayout from '../../components/layout/admin.layout';
-import { Button, createStyles, ScrollArea, Table } from '@mantine/core';
+import { Button, createStyles } from '@mantine/core';
 import {
   ArchiveOff,
   BuildingWarehouse,
@@ -8,7 +8,7 @@ import {
   PencilOff,
   Plus,
 } from 'tabler-icons-react';
-import React, { useEffect, useReducer, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { useDebouncedValue } from '@mantine/hooks';
 import { fetchRooms } from '../../redux/features/room/thunk/fetch-rooms';
@@ -17,39 +17,17 @@ import NoDataFound from '../../components/no-data-found';
 import TableHeader from '../../components/actions/table-header.component';
 import { TableBody } from '../../components/rooms/table-body.component';
 import TableFooter from '../../components/actions/table-footer.component';
-import DisableRoomModal from '../../components/rooms/disable-modal.component';
-import DeleteRoomModal from '../../components/rooms/delete-modal.component';
 import AddRoomModal from '../../components/rooms/add-modal.component';
+import DeleteRoomModal from '../../components/rooms/delete-modal.component';
+import DisableRoomModal from '../../components/rooms/disable-modal.component';
 import DownloadModal from '../../components/rooms/download-modal.compnent';
+import RoomInfoModal from '../../components/rooms/info-modal.component';
 import RestoreDisabledRoomModal from '../../components/rooms/restore-disabled.modal.component';
 import RestoreDeletedRoomModal from '../../components/rooms/restore-deleted.modal.component';
-import { PagingParams } from '../../models/pagination-params/paging-params.model';
-import { FormikValues, useFormik } from 'formik';
-import Header from '../../components/common/header.component';
-import RoomInfoModal from '../../components/rooms/info-modal.component';
 import UpdateModal from '../../components/rooms/update-modal.component';
-import { InputTypes } from '../../components/actions/models/input-type.constant';
-import { InputUpdateProps } from '../../components/actions/models/input-update-props.model';
-import { updateRoomById } from '../../redux/features/room/thunk/update-room-by-id';
+import { PagingParams } from '../../models/pagination-params/paging-params.model';
+import Header from '../../components/common/header.component';
 import { fetchRoomTypeNames } from '../../redux/features/room-type';
-
-import AddModal from '../../components/actions/modal/add-modal.component';
-import * as Yup from 'yup';
-import dayjs from 'dayjs';
-import { RoomType } from '../../models/room-type.model';
-import { PaginationResponse } from '../../models/pagination-response.payload';
-
-const UpdateRoomValidation = Yup.object().shape({
-  name: Yup.string()
-    .trim()
-    .min(1, 'Minimum room type name is 1 character')
-    .max(100, 'Maximum room type name is 100 characters.')
-    .required('Room type name is required'),
-  description: Yup.string().max(
-    500,
-    'Maximum room type description is 500 characters'
-  ),
-});
 
 const defaultPagination = {
   limit: 5,
@@ -129,6 +107,7 @@ function RoomsManagement(props: any) {
   const [id, setId] = useState<string>('');
   const [isInfoShown, setInfoShown] = useState<boolean>(false);
   const [isAddShown, setAddShown] = useState<boolean>(false);
+  const [isDownShown, setDownShown] = useState<boolean>(false);
   const [isUpdateShown, setUpdateShown] = useState<boolean>(false);
   const [isDeleteShown, setDeleteShown] = useState<boolean>(false);
   const [isRestoreDeletedShown, setRestoreDeletedShown] =
@@ -136,7 +115,6 @@ function RoomsManagement(props: any) {
   const [isDisableShown, setDisableShown] = useState<boolean>(false);
   const [isRestoreDisabledShown, setRestoreDisabledShown] =
     useState<boolean>(false);
-  const room = useAppSelector((state) => state.room.room);
 
   const ActionsFilter: React.FC = () => {
     return (
@@ -165,7 +143,7 @@ function RoomsManagement(props: any) {
         >
           <ArchiveOff />
         </Button>
-        <Button variant="outline" color="violet">
+        <Button variant="outline" color="violet" onClick={() => setDownShown(true)}>
           <Download />
         </Button>
       </>
@@ -193,94 +171,12 @@ function RoomsManagement(props: any) {
     },
   };
 
-//   const infoFields = [
-//     {
-//       label: 'Id',
-//       id: 'id',
-//       name: 'id',
-//       value: room.id,
-//       readOnly: true,
-//     },
-//     {
-//       label: 'Name',
-//       id: 'name',
-//       name: 'name',
-//       value: room.name,
-//       readOnly: true,
-//     },
-//     {
-//       label: 'Type',
-//       id: 'type',
-//       name: 'type',
-//       value: room.roomTypeName,
-//       readOnly: true,
-//     },
-//     {
-//       label: 'Create By',
-//       id: 'createdBy',
-//       name: 'createdBy',
-//       value: room.createdBy,
-//       readOnly: true,
-//     },
-//     {
-//       label: 'Create At',
-//       id: 'createdAt',
-//       name: 'createdAt',
-//       value: dayjs(room.createdAt).format('HH:mm DD/MM/YYYY'),
-//       readOnly: true,
-//     },
-//     {
-//       label: 'Update By',
-//       id: 'updatedBy',
-//       name: 'updatedBy',
-//       value: room.updatedBy,
-//       readOnly: true,
-//     },
-//     {
-//       label: 'Update At',
-//       id: 'updatedAt',
-//       name: 'updatedAt',
-//       value: dayjs(room.updatedAt).format('HH:mm DD/MM/YYYY'),
-//       readOnly: true,
-//     },
-//     {
-//       label: 'Description',
-//       id: 'description',
-//       name: 'description',
-//       value: room.description,
-//       readOnly: true,
-//     },
-//   ];
-
   const handleAddModalClose = () => {
     setAddShown(!isAddShown);
   };
 
-  const handleUpdateSubmit = (values: FormikValues) => {
-    dispatch(
-      updateRoomById({
-        id: values.id,
-        payload: values.payload,
-      })
-    )
-      .unwrap()
-      .then((e) => handleUpdateModalClose());
-  };
-
-  const updateFormik = useFormik({
-    validationSchema: UpdateRoomValidation,
-    initialValues: {
-      id: room.id,
-      name: room.name,
-      description: room.description,
-    },
-    enableReinitialize: true,
-    onSubmit: (e) => handleUpdateSubmit(e),
-  });
-
-  const handleUpdateModalClose = () => {
-    setUpdateShown(!isUpdateShown);
-    updateFormik.resetForm();
+  const handleDownModalClose = () => {
+    setDownShown(false);
   };
 
   return (
@@ -337,8 +233,8 @@ function RoomsManagement(props: any) {
               pagination={pagination}
             />
             <UpdateModal
-              formik={updateFormik}
-              handleSubmit={() => updateFormik.handleSubmit()}
+              // formik={updateFormik}
+              // handleSubmit={() => updateFormik.handleSubmit()}
               isShown={isUpdateShown}
               toggleShown={() => setUpdateShown(!isUpdateShown)}
               pagination={pagination}
@@ -348,7 +244,10 @@ function RoomsManagement(props: any) {
         ) : (
           <NoDataFound />
         )}
-
+        <DownloadModal
+          isShown={isDownShown}
+          toggleShown={() => handleDownModalClose()}
+        />
         <AddRoomModal
           isShown={isAddShown}
           pagination={pagination}
