25e06e89808ce41f7ad1d9c815a51c60b853702f
duynghim <74170051+duynghim@users.noreply.github.com>
2022-07-31 20:06:47 +0700

    Hot Fix

--- apps/mobile/src/app/redux/features/feedback/feedback.slice.ts
+++ apps/mobile/src/app/redux/features/feedback/feedback.slice.ts
@@ -1,32 +1,49 @@
-import AddNewFeedbackResponseModel from "../../models/add-new-feedback-response.model";
-import {createSlice} from "@reduxjs/toolkit";
-import {addNewFeedback} from "./thunk/Add-new-feedback.thunk";
-import {fetchFeedbacks} from "./thunk/fetch-feedbacks.thunk";
-import {FeedbackModel} from "../../models/feedback.model";
-import {fetchFeedbackById} from "./thunk/fetch-feedback-by-id.thunk";
+import AddNewFeedbackResponseModel from '../../models/add-new-feedback-response.model';
+import { createSlice } from '@reduxjs/toolkit';
+import { addNewFeedback } from './thunk/Add-new-feedback.thunk';
+import { fetchFeedbacks } from './thunk/fetch-feedbacks.thunk';
+import { FeedbackModel } from '../../models/feedback.model';
+import { fetchFeedbackById } from './thunk/fetch-feedback-by-id.thunk';
+import { addNewFeedbackAfterCheckout } from './thunk/Add-new-feedback-after-checkout.thunk';
+interface FeedbackResponse {
+  feedbackMessage: string;
+  rateNum: number;
+  bookingRoomId: string;
+  createdBy: string;
+  createdAt: string;
+  id: string;
+}
 
 interface FeedbackState {
   feedback: FeedbackModel;
   feedbacks: FeedbackFilterResponse[];
+  feedbackCheckout: FeedbackResponse;
 }
 
 const initialState: FeedbackState = {
   feedback: {} as FeedbackModel,
   feedbacks: [],
-}
+  feedbackCheckout: {} as FeedbackResponse,
+};
 
 const feedbackSlice = createSlice({
   name: 'feedback',
   initialState: initialState,
   reducers: {},
-  extraReducers: (builder  )=> {
-    builder.addCase(fetchFeedbackById.fulfilled, (state, {payload}) => {
+  extraReducers: (builder) => {
+    builder.addCase(fetchFeedbackById.fulfilled, (state, { payload }) => {
       state.feedback = payload;
     });
-    builder.addCase(fetchFeedbacks.fulfilled, (state, {payload}) => {
+    builder.addCase(fetchFeedbacks.fulfilled, (state, { payload }) => {
       state.feedbacks = payload;
     });
-  }
-})
+    builder.addCase(
+      addNewFeedbackAfterCheckout.fulfilled,
+      (state, { payload }) => {
+        state.feedbackCheckout = payload;
+      }
+    );
+  },
+});
 
-export const feedbackReducer = feedbackSlice.reducer
+export const feedbackReducer = feedbackSlice.reducer;
