2902002c4acaada094a3b857fb353c9f9002a3c9
Ngô Nguyên Bằng <webcam1362000@gmail.com>
2022-08-02 01:16:21 +0700

    Revert "Implement Quantity"

--- apps/mobile/src/app/redux/features/room-booking/thunk/attempt-checkin-booking-room.thunk.ts
+++ apps/mobile/src/app/redux/features/room-booking/thunk/attempt-checkin-booking-room.thunk.ts
@@ -4,19 +4,17 @@ import { API_URL } from '../../../../constants/constant';
 
 interface AttemptCheckinBookingRoomRequestPayload {
   id: string;
+  signature: string;
 }
-export const attemptCheckinBookingRoom = createAsyncThunk<
-  void,
-  AttemptCheckinBookingRoomRequestPayload,
-  {
-    rejectValue: {
-      message: string;
-    };
+export const attemptCheckinBookingRoom = createAsyncThunk<void, AttemptCheckinBookingRoomRequestPayload, {
+  rejectValue: {
+    message: string;
   }
->('room-booking/attempt-checkin', async (payload, thunkAPI) => {
-  return await axiosPostAPICall(
-    `${API_URL}/booking-room/attempt-checkin/${payload.id}`,
-    {},
-    thunkAPI
-  );
-});
+}>(
+  'room-booking/attempt-checkin',
+  async (payload, thunkAPI) => {
+    return await axiosPostAPICall(`${API_URL}/booking-room/attempt-checkin/${payload.id}`, {
+      signature: payload.signature
+    }, thunkAPI);
+  }
+);
