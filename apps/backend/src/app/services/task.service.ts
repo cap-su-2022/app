bb9be4be3c5cd35eea2c0cb5548d31ca85052008
longdemon <longnv031120@gmail.com>
2022-08-18 17:17:08 +0700

    Auto cancel affter 24h

--- apps/backend/src/app/services/task.service.ts
+++ apps/backend/src/app/services/task.service.ts
@@ -1,14 +1,18 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression, Interval } from '@nestjs/schedule';
 import { BookingRoomRepository } from '../repositories';
 import { BookingRoomStatus } from '../enum/booking-room-status.enum';
 import * as dayjs from 'dayjs';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class TasksService {
   private readonly logger = new Logger(TasksService.name);
 
-  constructor(private readonly repositoryBooking: BookingRoomRepository) {}
+  constructor(
+    private readonly repositoryBooking: BookingRoomRepository,
+    private readonly dataSource: DataSource
+  ) {}
 
   //@Cron(CronExpression.EVERY_10_SECONDS)
   async handleCheckRoomBookingStillInProgress() {
@@ -20,14 +24,63 @@ export class TasksService {
     );
   }
 
-  // @Cron(CronExpression.EVERY_10_SECONDS)
+  @Cron(CronExpression.EVERY_HOUR)
   async handleAutoCancelBookedRequest() {
     const currDate = dayjs(new Date()).format('YYYY-MM-DD');
     const currTime = dayjs(new Date()).format('HH:mm:ss');
-    const result = await this.repositoryBooking.getRequestBookedInDay(currDate);
-    result.map((re) => {
-      if (re.timeEnd < currTime) {
+    try {
+      const result = await this.repositoryBooking.getRequestBookedInDay(
+        currDate
+      );
+      if (result.length > 0) {
+        result.forEach(async (request) => {
+          const reason =
+            'Check-in time has been exceeded. Your request was automatically canceled';
+          if (request.timeEnd < currTime) {
+            await this.repositoryBooking.cancelRoomBookingByIdNoQueryRunner(
+              null,
+              request.id,
+              reason,
+              'System Admin'
+            );
+          }
+        });
       }
-    });
+    } catch (e) {
+      this.logger.error(e.message);
+      throw new BadRequestException(
+        e.message || 'Error while getting booking rooms'
+      );
+    }
+  }
+
+  @Cron(CronExpression.EVERY_HOUR)
+  async handleAutoCancelPendingRequest() {
+    const currDate = dayjs(new Date());
+
+    try {
+      const result = await this.repositoryBooking.getAllRequestPending();
+      if (result.length > 0) {
+        result.forEach(async (request) => {
+          const reason =
+            'Your request has been pending for more than 24 hours, automatically canceled';
+
+          if (currDate.diff(request.requestedAt) >= 24 * 60 *60 * 1000) {
+            await this.repositoryBooking.cancelRoomBookingByIdNoQueryRunner(
+              null,
+              request.id,
+              reason,
+              'System Admin'
+            );
+          }
+          
+        });
+      }
+    } catch (e) {
+      this.logger.error(e.message);
+      throw new BadRequestException(
+        e.message || 'Error while getting booking rooms'
+      );
+    }
   }
 }
