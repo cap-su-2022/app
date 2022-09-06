54e0341069c17351f2cd239471c70645f5cd7c1e
cyrus <kazuocyrus@gmail.com>
2022-09-06 19:40:21 +0700

    Delete remaining wishlist files...

--- apps/backend/src/app/repositories/room-wishlist-hist.repository.ts
+++
@@ -1,6 +0,0 @@
-import { Repository } from 'typeorm';
-import { CustomRepository } from '../decorators/typeorm-ex.decorator';
-import { RoomWishlistHist } from '../models';
-
-@CustomRepository(RoomWishlistHist)
-export class RoomWishlistHistRepository extends Repository<RoomWishlistHist> {}
